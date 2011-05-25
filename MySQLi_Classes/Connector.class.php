9e95d617bee7eafdfb539c6e16d4d3dd9d12a16c
Anders Ingemann <anders@ingemann.de>
2011-05-25 16:11:39 +0200

    Some small fixes

--- MySQLi_Classes/Connector.class.php
+++ MySQLi_Classes/Connector.class.php
@@ -1,5 +1,7 @@
 <?php
 namespace MySQLi_Classes;
+use MySQLi_Classes\Exceptions\ErrorException;
+
 use MySQLi_Classes\Exceptions\ConnectionException;
 use MySQLi_Classes\Queries\Query;
 use MySQLi_Classes\Statements\Statement;
@@ -12,7 +14,7 @@ class Connector {
 		if(isset(self::$mysqli))
 			throw new ConnectionException('Already connected to a server.');
 		if($mysqli->connect_errno != 0)
-			throw new ConnectionException('The MySQLi instance is not connected to a database.');
+			throw ErrorException::findClass($mysqli, __LINE__);
 		if($mysqli->server_version == '')
 			throw new ConnectionException('The MySQLi instance is not connected to a database.');
 		self::$mysqli = $mysqli;
