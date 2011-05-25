63b159d22f849cd1caa9418495d8d422c69b1299
Anders Ingemann <anders@ingemann.de>
2011-05-25 16:06:12 +0200

    Changed the exceptions to handle conn. errors

--- MySQLi_Classes/Exceptions/ErrorException.class.php
+++ MySQLi_Classes/Exceptions/ErrorException.class.php
@@ -1,5 +1,8 @@
 <?php
 namespace MySQLi_Classes\Exceptions;
+use MySQLi_Classes\Exceptions\Client as C;
+use MySQLi_Classes\Exceptions\Server as S;
+use MySQLi_Classes\Exceptions\Server\AccessDenied as AD;
 class ErrorException extends \RuntimeException implements Exception {
 	
 	public function __construct($message = "", $code = 0, $previous = null, $line = null) {
@@ -12,15 +15,37 @@ class ErrorException extends \RuntimeException implements Exception {
 	 * Depending on the error code, this method returns either a ServerException or a ClientException.
 	 * If no error code fits, it returns a new instance of itself
 	 * @param mysqli $mysqli The MySQLi instance where the error occurred
-	 * @return MySQLErrorException
+	 * @return MySQLi_Classes\Exceptions\ErrorException
 	 */
 	public static function findClass(\mysqli $mysqli, $line) {
-		if(1000 <= $mysqli->errno && $mysqli->errno < 2000) {
-			$exception = Server\ServerErrorException::findClass($mysqli, $line);
-		} elseif($mysqli->errno >= 2000) {
-			$exception = Client\ClientErrorException::findClass($mysqli, $line);
+		if($mysqli->connect_errno > 0) {
+			$errno = $mysqli->connect_errno;
+			$error = $mysqli->connect_error;
 		} else {
-			$exception = new ErrorException($mysqli->error, $mysqli->errno, null, $line);
+			$errno = $mysqli->errno;
+			$error = $mysqli->error;
+		}
+		if(1000 <= $errno && $errno < 2000) {
+			switch($errno) {
+				case 1044: return new Server\AccessDenied\DatabaseAccessDeniedException($error, $errno, null, $line);
+				case 1045: return new Server\AccessDenied\ServerAccessDeniedException($error, $errno, null, $line);
+				case 1048: return new Server\BadNullException($error, $errno, null, $line);
+				case 1054: return new Server\BadFieldException($error, $errno, null, $line);
+				case 1062: return new Server\DuplicateEntryException($error, $errno, null, $line);
+				case 1064: return new Server\ParseErrorException($error, $errno, null, $line);
+				case 1095: return new Server\AccessDenied\KillDeniedException($error, $errno, null, $line);
+				case 1142: return new Server\AccessDenied\TableAccessDeniedException($error, $errno, null, $line);
+				case 1143: return new Server\AccessDenied\ColumnAccessDeniedException($error, $errno, null, $line);
+				case 1149: return new Server\SyntaxErrorException($error, $errno, null, $line);
+				case 1227: return new Server\AccessDenied\SpecificAccessDeniedException($error, $errno, null, $line);
+				case 1364: return new Server\NoDefaultForFieldException($error, $errno, null, $line);
+				case 1370: return new Server\AccessDenied\ProcedureAccessDeniedException($error, $errno, null, $line);
+				default:   return new Server\UnknownErrorException($error, $errno, null, $line);
+			}
+		} elseif($errno >= 2000) {
+			return new ClientErrorException($error, $errno, null, $line);
+		} else {
+			$exception = new ErrorException($error, $errno, null, $line);
 		}
 		return $exception;
 	}
